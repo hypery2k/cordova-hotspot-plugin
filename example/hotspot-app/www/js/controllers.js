0b0d4ed01a0834c3f9d22f139a2e5ecf6851aedc
Martin Reinhardt <contact@martinreinhardt-online.de>
2016-04-18 13:42:31 +0200

    Update sample app

--- example/hotspot-app/www/js/controllers.js
+++ example/hotspot-app/www/js/controllers.js
@@ -1,83 +1,83 @@
 angular.module('starter.controllers', [])
-    .controller('HotSpotCtrl', function ($scope, $ionicLoading, $timeout) {
+  .controller('HotSpotCtrl', function ($scope, $ionicLoading, $timeout) {
 
-        var init = function () {
-            $ionicLoading.show();
-            // default to WPA2PSK
-            $scope.config = {
-              mode: 'WPA2PSK'
-            };
-            cordova.plugins.hotspot.isHotspotEnabled(
-                function () {
-                    $ionicLoading.hide();
-                    $scope.isHotSpotActive = true;
-                }, function () {
-                    $ionicLoading.hide();
-                    $scope.isHotSpotActive = false;
-                }
-            );
-        };
+    var init = function () {
+      $ionicLoading.show();
+      // default to WPA2PSK
+      $scope.config = {
+        mode: 'WPA2PSK'
+      };
+      cordova.plugins.hotspot.isHotspotEnabled(
+        function () {
+          $ionicLoading.hide();
+          $scope.isHotSpotActive = true;
+        }, function () {
+          $ionicLoading.hide();
+          $scope.isHotSpotActive = false;
+        }
+      );
+    };
 
-        // init controllers
-        init();
+    // init controllers
+    init();
 
-        // API
-        $scope.start = function () {
-          $ionicLoading.show();
-            cordova.plugins.hotspot.createHotspot(
-                $scope.config.ssid, $scope.config.mode, $scope.config.password,
-                function () {
-                  // delay UI refresh
-                  $timeout(function () {
-                    $ionicLoading.hide();
-                    init();
-                  }, 500);
-                }, function () {
-                    $ionicLoading.hide();
-                    alert('Hotspot creation failed');
-                }
-            );
-        };
+    // API
+    $scope.start = function () {
+      $ionicLoading.show();
+      cordova.plugins.hotspot.createHotspot(
+        $scope.config.ssid, $scope.config.mode, $scope.config.password,
+        function () {
+          // delay UI refresh
+          $timeout(function () {
+            $ionicLoading.hide();
+            init();
+          }, 500);
+        }, function () {
+          $ionicLoading.hide();
+          alert('Hotspot creation failed');
+        }
+      );
+    };
 
-        $scope.stop = function () {
-          $ionicLoading.show();
-          cordova.plugins.hotspot.stopHotspot(
-              function () {
-                  $ionicLoading.hide();
-                  $scope.isHotSpotActive = false;
-                  init();
-              }, function () {
-                  alert('AP disabling failed');
-              }
+    $scope.stop = function () {
+      $ionicLoading.show();
+      cordova.plugins.hotspot.stopHotspot(
+        function () {
+          $ionicLoading.hide();
+          $scope.isHotSpotActive = false;
+          init();
+        }, function () {
+          alert('AP disabling failed');
+        }
+      );
+    };
+  })
+  .controller('DevicesCtrl', function ($scope) {
+    var init = function () {
+      cordova.plugins.hotspot.isHotspotEnabled(
+        function () {
+          $scope.status = 'Reading devices ...';
+          cordova.plugins.hotspot.getAllHotspotDevices(
+            function (response) {
+              $scope.status = false;
+              $scope.devices = response;
+            }, function () {
+              $scope.status = false;
+              alert('Device listing failed.');
+            }
           );
-        };
-    })
-    .controller('DevicesCtrl', function ($scope, $ionicLoading) {
-        var init = function () {
-            $ionicLoading.show();
-            cordova.plugins.hotspot.isHotspotEnabled(
-                function () {
-                    cordova.plugins.hotspot.getAllHotspotDevices(
-                        function (response) {
-                            $ionicLoading.hide();
-                            $scope.devices = response;
-                        }, function () {
-                            $ionicLoading.hide();
-                            alert('Device listing failed.');
-                        }
-                    );
-                }, function () {
-                    $ionicLoading.hide();
-                    $scope.isHotSpotActive = false;
-                }
-            );
-        };
+        }, function () {
+          $scope.status = false;
+          $scope.isHotSpotActive = false;
+        }
+      );
+    };
 
-        // init controllers
-        init();
+    // init controllers
+    init();
 
-        // API
-        $scope.refresh = function () {
-          init()
-        };
-    });
+    // API
+    $scope.refresh = function () {
+      init()
+    };
+  });
